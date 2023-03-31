9da486e12d01d86dda1206b87d8b41c87a20d4c4
Andrew <anolte512@gmail.com>
2023-03-31 16:45:58 -0400

    sort bindings

---
+++ mac/sort_keybindings.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+def main():
+    with open(sys.argv[1]) as f:
+        bindings = json.loads('\n'.join([line for line in f.readlines() if not line.startswith('//')]))
+    bindings = sorted(bindings, key=lambda obj: obj['key'])
+
+
+
+    with open(sys.argv[1], 'w') as f:
+        f.write(json.dumps(bindings))
+    
+    # print(json.dumps(bindings))
+
+
+if __name__ == "__main__":
+    main()
